164f6cef1f4e9e4b886390ec8f2cba0bc1e80490
Jing Jing <JingJing.22@ichat.sp.edu.sg>
2023-05-04 11:03:07 +0800

    New changes in the github repository

--- bmi.py
+++ bmi.py
@@ -1,12 +1,15 @@
 def calculate_bmi(height,weight) :
     print("Height = " + str(height))
     print("Weight = " + str(weight))
-    bmi=weight/(height*height)
+    bmi = weight/(height*height)
     print("BMI = " + str(bmi))
-    if bmi<18.5:
+    if bmi < 18.5:
         print("The user is Under Weight")
-    elif bmi>=18.5 and bmi<=25.0:
+        return -1
+    elif bmi >= 18.5 and bmi <= 25.0:
         print("The user is Normal Weight")
+        return 0
     else:
         print("The user is Over Weight")
+        return 1
 calculate_bmi(weight=57,height=1.73)
